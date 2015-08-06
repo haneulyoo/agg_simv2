7c7a124820b49d1bf90b70cb374c84e51860fb63
ctriandafillou <cat@nsit-dhcp-186-187.uchicago.edu>
2015-08-06 14:36:52 -0500

    Draft 1 done
    Creation/destruction, v limited testing. Plots

--- gsim.py
+++ gsim.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 """
 Basic implementation of the Gillespie algorithm.
-__author__
+
+Designed for unimolecular creation/destruction reactions
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Species(object):
-    """A chemical species. Has an inherent count."""
+    """A chemical species. Has an inherent property count."""
     def __init__(self, count):
         self.count = count
     
@@ -31,16 +33,32 @@ class Reaction(object):
         self.ip = ip
         self.op = op
         
-    def prop(self, ip, rate):
+    def prop(self):
         alpha = 1
-        for i in ip:
+        for i in self.ip:
             alpha *= i.count
-        return alpha*rate
+        return alpha*self.rate
+        
+    def perform(self, ip, op, rate):
+        #insert specific reaction details here
+        pass
         
 class ConstInduction(Reaction):
     """Specific Reaction instance with a rate independent of species concentration"""
-    def prop(self, rate):
-        return rate
+    def prop(self):
+        return self.rate
+        
+    def perform(self):
+        #quick fix now, better fix later:
+        self.op[0].produce()
+        
+class UniDeg(Reaction):
+    """Specific Reaction instance of degredation with a rate dependent on the
+    concentration of the species being degraded.
+    """
+    def perform(self):
+        #quick fix now, better fix later
+        self.ip[0].destroy()
         
         
 class Network(object):
@@ -54,25 +72,59 @@ class Network(object):
         self.species = sp_list
         self.reactions = rxn_list
         
+    def update_species(self, sp_list, rxn_list, alpha_vec, rn):
+        pass
+        
     def simulate(self, t_max, file_path):
         """Still very exploratory/could be crashy. Make sure to double check
         rate/times so that the simulation doesn't get out of control.
+        Current implementation is specifically for two unimolecular creation/
+        destruction reactions.
         """
-        f.open(file_path, "w")
-        f.write(str(self.species) + "\n")
+        #f = open(file_path, "w")
+        #f.write(str(self.species) + "\n")
         t = 0
         n_steps = 0
+        #temporary data storage:
+        data = []
+        time = []
+        print self.species
         while t < t_max:
+            #again, generalization
+            data.append(self.species[0].count)
+            time.append(t)
             r1, r2 = np.random.uniform(), np.random.uniform()
-            alpha = 0
-            for i in self.reactions:
-                alpha += i.prop()
+            alpha_vec = [i.prop() for i in self.reactions]
+            alpha = np.sum(alpha_vec)
             try:
-                tau = 1./alpha
+                tau = 1./alpha*np.log(1./r1)
             except ZeroDivisionError:
-                print "Propensity equal to zero at step: %d, time: %d" % (n_steps, t)
+                print "Propensity equal to zero at step = %d, time = %d" % (n_steps, t)
+            t = t + tau
+            n_steps += 1
+            #update species here - could generalize much much more:
+            if r2 < 1./alpha:
+                self.reactions[0].perform()
+            else:
+                self.reactions[1].perform()
+            if n_steps % 10 == 0:
+                print "Number of steps = %d" % n_steps
             
-       print "Simulation finished after %d steps" % n_steps     
-       f.close() 
+        print "Simulation finished after %d steps" % n_steps     
+        #f.close()
+        return time, data
         
-#def main(sp_dict, )
+def main():
+    L = Species(0)
+    arrival = ConstInduction("Induction", None, [L], 1)
+    jump = UniDeg("Degredation", [L], None, 0.1)
+    cliff = Network([L], [arrival, jump])
+    x, y = cliff.simulate(100, "dummy_file")
+    #plots
+    plt.step(x, y)
+    plt.xlabel("Time (s)")
+    plt.ylabel("Number of Lemmings")
+    plt.show()  
+    
+if __name__ == "__main__":
+    main()
