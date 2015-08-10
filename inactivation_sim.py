9e433e69bf33d333ce19f3d422c52c34c973fa0e
ctriandafillou <cat@nsit-dhcp-186-187.uchicago.edu>
2015-08-10 13:31:57 -0500

    i/o
    output now saved in numpy array (can be written to file). some progress
    in inactivation as well.

--- inactivation_sim.py
+++ inactivation_sim.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python
+'''
+Simulates temperature-dependent aggregation where a nucleation step
+(approximated by dimerization of the species in a temperature dependent fashion)
+and dimers are subsequently inactivated at a rate proportional to the square
+root of the number of inactivated species present. 
+'''
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Aug  7 15:26:02 2015
-
-@author: Triandafillou
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,10 +17,19 @@ class Inactivation(gsim.Reaction):
     the concentration of previously inactivated species.
     
     A -> iA
+    Generic rate is in units s^-1; the propensity of the reaction is dependent
+    linearly on the number of activated molecules, and also on the square root
+    of the number of inactivated molecules, which roughly approximates the
+    surface area of the growing aggregated (inactivated) bulk species.
     """
-    def perform(self, ip, op, rate):
+    #this could be recoded to change an internal state rather than introducing
+    #a new species?
+    def prop(self):
+        return self.rate * self.ip[0].count * np.sqrt(self.op[0].count)
+    
+    def perform(self, ip, op):
         self.ip[0].destroy()
-        self.op[0].destroy()
+        self.op[0].create()
 
         
 class Temp_Dimerization(gsim.Reaction):
@@ -27,12 +38,33 @@ class Temp_Dimerization(gsim.Reaction):
 
     A + A -> AA
     Input should be a list of one species (in the above example A) and the
-    output should be a list of a single dimerized species)
+    output should be a list of a single dimerized species).
     """
+    
+    def __init__(self, name, ip, op, rate, T):
+        self.name = name
+        self.rate = rate
+        self.ip = ip
+        self.op = op
+        self.temp = T
+        
     def prop(self):
-        return self.count*(self.count-1)*self.rate
+        return self.count*(self.count-1)*self.rate*T
     
     def perform(self, ip, op, rate):
         self.ip[0].destroy()
         self.ip[0].destroy()
         self.op[0].produce()
+        
+        
+def main():
+    # Species in the system
+    A = gsim.Species("A", 100)
+    AA = gsim.Species("AA", 0)
+    iAA = gsim.Species("iAA", 0)
+    # Possible reactions
+    nuc = Temp_Dimerization("Nucleation", [A], [AA], 0.1)
+    inactivation = Inactivation("Inactivation", [AA], [iAA], 1.0)
+    # System Loop
+    system = gsim.Network([A, AA, iAA], [nuc, inactivation])
+    x, y = system.simulate()
--- lemming.py
+++ lemming.py
@@ -1,9 +1,5 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Aug  7 15:34:40 2015
-
-@author: Triandafillou
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,19 +13,18 @@ def main():
     arrival = ConstInduction("Induction", None, [L], 1.0)
     jump = UniDeg("Degredation", [L], None, 0.1)
     cliff = Network([L], [arrival, jump])
-    x, y = cliff.simulate(200, "dummy_file")
-
-    print np.mean(y), np.mean(y)/np.var(y)
-    plt.subplot(121)
-    plt.step(x, y)
-    plt.title("Lemmings vs. time")
-    plt.xlabel("Time (s)")
-    plt.ylabel("Number of Lemmings")
-    plt.subplot(122)
-    plt.title("Lemming count distribution")
-    plt.hist([i[0] for i in y], normed=True)
-    plt.xlabel("Lemming population")
-    plt.show()
+    cliff.simulate(200, "dummy_file2.dat")
+    #print np.mean(y), np.mean(y)/np.var(y)
+    #plt.subplot(121)
+#    plt.step(x, y)
+#    plt.title("Lemmings vs. time")
+#    plt.xlabel("Time (s)")
+#    plt.ylabel("Number of Lemmings")
+#    plt.subplot(122)
+#    plt.title("Lemming count distribution")
+#    plt.hist([i[0] for i in y], normed=True)
+#    plt.xlabel("Lemming population")
+#    plt.show()
     
 if __name__ == "__main__":
     main()
