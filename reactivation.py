cbdeacc821ba265564a5f112092cd299ad016a0c
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2015-08-11 08:47:25 -0500

    started reactivation

---
+++ reactivation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+'''
+Simulates reactivation of deactivated species.
+'''
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import gsim
+
+class Reactivation(gsim.Reaction):
+    pass
