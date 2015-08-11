0cd129d6179fa205b3f558d70b416ed8c88084b0
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2015-08-11 11:06:48 -0500

    final rate dependencies added
    still need to play with parameters

--- reactivation.py
+++ reactivation.py
@@ -6,8 +6,8 @@ Simulates reactivation of deactivated species in a temperature dependent fashion
 
 import numpy as np
 import matplotlib.pyplot as plt
-from gsim import Reaction, Species, Network
-from inactivation_sim import Inactivation, Temp_Dimerization
+from gsim import Reaction, Species, Network, UniDeg
+from inactivation_sim import Temp_Dimerization
 
 class Reactivation(Reaction):
     """A reaction which converts a single inactivated species to two active.
@@ -26,6 +26,8 @@ class HeatInducedProduction(Reaction):
     """A reaction where the rate of production is temperature dependent.
     
     0 -> A
+    Also dependent on the concentration of inactivated molecules in the
+    simulation. Include this parameter in the ip list.
     """
     def __init__(self, name, ip, op, rate, T):
         self.name = name
@@ -35,32 +37,78 @@ class HeatInducedProduction(Reaction):
         self.temp = T
    
     def prop(self):
-        return self.rate*self.temp
+        return self.rate*self.temp*self.ip[0].count
         
     def perform(self):
         self.op[0].produce()
         
+class HeatInducedInactivation(Reaction):
+    """A reaction in which a species is inactivated at a rate proportional to
+    the concentration of previously inactivated species and the temperature.
+    
+    A -> iA
+    Generic rate is in units s^-1; the propensity of the reaction is dependent
+    linearly on the number of activated molecules, and also on the square root
+    of the number of inactivated molecules, which roughly approximates the
+    surface area of the growing aggregated (inactivated) bulk species.
+    """    
+    def __init__(self, name, ip, op, rate, T):
+        self.name = name
+        self.rate = rate
+        self.ip = ip
+        self.op = op
+        self.temp = T
+    
+    def prop(self):
+        if self.op[0].count == 0:
+            return self.rate*self.ip[0].count
+        else:
+            return self.rate*self.ip[0].count*np.sqrt(self.op[0].count)*self.temp
+    
+    def perform(self):
+        self.ip[0].destroy()
+        self.op[0].produce()
+        
+        
 def main():
+    """Simulate temperature dependent aggregation and disaggregation.
+    
+    Clearly need to explicitly write temperature into the simulate method in
+    the future.
+    """
     # Species
     A = Species("A", 100)
     AA = Species("AA", 0)
     iAA = Species("iAA", 0)
-    C = Species("C", 0)
+    C = Species("C", 10)
     # Temperature independent reactions
-    inactivation = Inactivation("Inactivation", [AA], [iAA], 0.005)
-    disagg = Reactivation("Disagg", [iAA, C], [A, C], 0.005)
+    disagg = Reactivation("Disagg", [iAA, C], [A, C], 0.01)
+    deg = UniDeg("C Degredation", [C], [None], 0.1)
     # Simulation and Temperature-Dependent Reaction
     # Temperature ramp = 300, 320, 300
-    nuc = Temp_Dimerization("Nucleation", [A], [AA], 0.00001, 300)
-    hip = HeatInducedProduction("Heat Induced Production", [None], [C], 0.001, 300)
+    nuc = Temp_Dimerization("Nucleation", [A], [AA], 0.0001, 300)
+    hip = HeatInducedProduction("Heat Induced Production", [iAA], [C], 0.001, 300)
+    inactivation = HeatInducedInactivation("Inactivation", [AA], [iAA], 0.01, 10)
     sp_list = [A, AA, iAA, C]
-    system = Network(sp_list, [nuc, inactivation, disagg, hip])
-    x = system.simulate(0, 50, "None")
+    rxn_list= [nuc, inactivation, disagg, hip, deg]
+    system = Network(sp_list, rxn_list)
+    x = system.simulate(0, 20, "None")
+    nuc = Temp_Dimerization("Nucleation", [A], [AA], 0.0001, 320)
+    hip = HeatInducedProduction("Heat Induced Production", [iAA], [C], 0.001, 320)
+    inactivation = HeatInducedInactivation("Inactivation", [AA], [iAA], 0.01, 30)
+    y = system.simulate(x[-1,0], 50, "None")
+    nuc = Temp_Dimerization("Nucleation", [A], [AA], 0.0001, 300)
+    hip = HeatInducedProduction("Heat Induced Production", [iAA], [C], 0.001, 300)
+    inactivation = HeatInducedInactivation("Inactivation", [AA], [iAA], 0.01, 10)
+    z = system.simulate(y[-1,0], 100, "None")
+    
+    final = np.vstack((x, y, z))
     
     fig, axis = plt.subplots()    
     for i in range(1,len(sp_list)+1):
-        axis.step(x[:,0], x[:,i], label=sp_list[i-1].name)
+        axis.step(final[:,0], final[:,i], label=sp_list[i-1].name)
     plt.legend()
+    plt.xlim(0,final[-1,0])
     plt.show()
     
 if __name__ == "__main__":
