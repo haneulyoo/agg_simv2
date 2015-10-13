11d84e59fff1d68a4dc2bd1da544a011aaa00a6a
ctriandafillou <ctriandafillou@uchicago.edu>
2015-10-13 17:36:23 -0500

    more progress

--- pabchaperone_translation.py
+++ pabchaperone_translation.py
@@ -49,15 +49,15 @@ class CProduction(Reaction):
     def perform(self):
         self.op[0].produce()
         
-class CDegradation(Reaction):
-    """Chaperone degradation reaction.
-    
-    C -> 0
-    
-    Rate is a general protein degradation rate.
-    """
-    def perform(self):
-        self.ip[0].destroy()
+#class CDegradation(Reaction):
+#    """Chaperone degradation reaction.
+#    
+#    C -> 0
+#    
+#    Rate is a general protein degradation rate.
+#    """
+#    def perform(self):
+#        self.ip[0].destroy()
         
         
         
@@ -65,31 +65,33 @@ def main():
     # Rates
     k1 = .01 # Deactivation
     k2 = .01 # Reactivation
-    k3 = .01 # Protein synthesis
+    k3 = 1 # Protein synthesis
     k4 = .01 # Protein degradation    
-    temp_fxn = [(0,298)]
+    temp_fxn = [(0,100)]
     # Species
     Pab1 = Species("Pab1", 100)
-    C = Species("Chaperone", 0)
+    C = Species("Chaperone", 50)
     iPab1 = Species("iPab1", 0)
     # Reactions
     aggregation = Pab1Deactivation("agg", [Pab1], [iPab1], None, k1)
     disaggregation = Pab1Reactivation("disagg", [iPab1, C], [Pab1, C], None, k2)
     Ctranslation = CProduction("C Translation", [Pab1], [C], None, k3)
-    Cdeg = CDegradation("C Degradation", [C], [], None, k4)
+    Cdeg = UniDeg("C Degradation", [C], [], None, k4)
     # System
     species_list = [Pab1, C, iPab1]
     rxn_list = [aggregation, disaggregation, Ctranslation, Cdeg]
     system = Network(species_list, rxn_list)
     # Simulation
-    x = system.simulate(0, 1000, temp_fxn, None)
+    x = system.simulate(0, 100, temp_fxn, None)
+    print x[:,3]
     # Plots
     colors = ['royalblue', 'gold', 'firebrick']
     fig, (axis2, axis) = plt.subplots(2, 1, figsize=(7,7))
     for i in range(2,len(species_list)+2):
-        axis.plot(x[:100,0], x[:100,i], label=species_list[i-2].name, c=colors[i-2], linewidth=3)
+        axis.plot(x[:,0], x[:,i], label=species_list[i-2].name, c=colors[i-2], linewidth=3)
     axis.legend(loc=0)
-    axis.set_xlim(0,1)#x[-1,0])
+    axis.set_xlim(0,x[-1,0]+5)
+    axis.set_ylim(-2, 102)
     axis.set_xlabel("Time")
     axis.set_ylabel("Molecular species count")
     axis2.step(x[:, 0], x[:, 1], linewidth=3)
