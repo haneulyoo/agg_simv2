4708d3dfe3cc0b67cdfaf5c9c595371518107b09
ctriandafillou <ctriandafillou@uchicago.edu>
2015-10-16 10:06:51 -0500

    added ODE method
    Basic implementation, uses scipy odeint package for integration

--- pabctranslation_ODE.py
+++ pabctranslation_ODE.py
@@ -14,39 +14,69 @@ from scipy.integrate import odeint
 
 def deriv(z, t):
     k1 = .1 # Deactivation
-    k2 = .1 # Reactivation
-    k3 = 1 # Protein synthesis
-    k4 = .1 # Protein degradation
-    T = 1
+    k2 = .02 # Reactivation
+    k3 = .03 # Protein synthesis
+    k4 = .01 # Protein degradation
     
     dPab = -k1*z[0]*T + k2*z[1]*z[2]
     diPab = -k2*z[1]*z[2] + k1*z[0]*T
     dC = k3*z[1] - k4*z[2]    
     
     return np.array([dPab, diPab, dC])
-                    
-time = np.arange(0, 100, 1)
-yinit = np.array([100., 0., 50])
-z = odeint(deriv, yinit, time)
 
+T = 1                  
+time1 = np.arange(0, 100, 1)
+zinit = np.array([100., 0., 20])
+z1 = odeint(deriv, zinit, time1)
+
+T = 25
+time2 = np.arange(100, 150, 1)
+z2 = odeint(deriv, z1[-1], time2)
+
+T = 1
+time3 = np.arange(150, 200, 1)
+z3 = odeint(deriv, z2[-1], time3)
+
+times = np.concatenate((time1, time2, time3))
+final = np.vstack((z1, z2, z3))
+
+
+
+
+
+
+# Plots
 names = ['Pab1', 'iPab1', 'C']
 colors = ['royalblue', 'firebrick', 'gold']
 for i in xrange(3):
-    plt.plot(time, z[:, i], label=names[i], c=colors[i], linewidth=2)
+    plt.plot(times, final[:, i], label=names[i], c=colors[i], linewidth=2)
 plt.xlabel('t')
-plt.ylabel('Species Count')
+plt.ylabel('Species Concentration')
 plt.legend(loc=0)
 plt.show()
 
-    
-def deriv2(z, t):
-    k1 = .2
-    k2 = .1
 
-    dA = -1*k1*z[0] - k2*z[0]
-    dB = k1*z[0]
-    dC = k2*z[0]
-    return np.array([dA, dB, dC])
+
+
+
+
+
+
+
+
+
+
+
+
+    
+#def deriv2(z, t):
+#    k1 = .2
+#    k2 = .1
+#
+#    dA = -1*k1*z[0] - k2*z[0]
+#    dB = k1*z[0]
+#    dC = k2*z[0]
+#    return np.array([dA, dB, dC])
     
 #time = np.arange(0, 50, 1)
 #zinit = np.array([100., 0., 0.])
