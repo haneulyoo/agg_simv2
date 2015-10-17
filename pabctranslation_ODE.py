44c6dd6f68591f2190f3d1cd7806f174ee638afa
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2015-10-17 12:33:38 -0500

    parameters
    searching for realistic constants

--- pabctranslation_ODE.py
+++ pabctranslation_ODE.py
@@ -12,29 +12,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
+# Constants
+
+V = 37 #units: cubic microns; do cells change size during heat stress? probably
+# protein abundance -> using amounts from Kulak et al 2014 becuase its a methods paper
+total_Pab = 95624. # molecules/cell
+total30_HSP104 = 52655. # molecules/cell, should be roughly double that 15-60
+                       # minutes of mild heat shock according to Kawai 1999
+
 def deriv(z, t):
-    k1 = .1 # Deactivation
-    k2 = .02 # Reactivation
-    k3 = .03 # Protein synthesis
+    k1 = 10. # base Deactivation rate
+    k2 = 1. # Reactivation rate
+    k3 = .1 # Protein synthesis
     k4 = .01 # Protein degradation
     
-    dPab = -k1*z[0]*T + k2*z[1]*z[2]
-    diPab = -k2*z[1]*z[2] + k1*z[0]*T
+    dPab = -1*k1*z[0]*deltaT + k2*z[1]*z[2]
+    diPab = k1*z[0]*deltaT - k2*z[1]*z[2]
     dC = k3*z[1] - k4*z[2]    
     
     return np.array([dPab, diPab, dC])
 
-T = 1                  
-time1 = np.arange(0, 100, 1)
-zinit = np.array([100., 0., 20])
+deltaT = 1                  
+time1 = np.arange(0, 50, .1)
+#zinit = np.array([.85*total_Pab, .15*total_Pab, total30_HSP104])
+zinit = np.array([85, 15, 20])
 z1 = odeint(deriv, zinit, time1)
 
-T = 25
-time2 = np.arange(100, 150, 1)
+deltaT = 10
+time2 = np.arange(50, 100, .1)
 z2 = odeint(deriv, z1[-1], time2)
 
-T = 1
-time3 = np.arange(150, 200, 1)
+deltaT = 1
+time3 = np.arange(100, 150, .1)
 z3 = odeint(deriv, z2[-1], time3)
 
 times = np.concatenate((time1, time2, time3))
@@ -50,9 +59,11 @@ names = ['Pab1', 'iPab1', 'C']
 colors = ['royalblue', 'firebrick', 'gold']
 for i in xrange(3):
     plt.plot(times, final[:, i], label=names[i], c=colors[i], linewidth=2)
+#plt.plot(times, np.ones(len(times))*total_Pab, c='k')
 plt.xlabel('t')
-plt.ylabel('Species Concentration')
+plt.ylabel('Species Count')
 plt.legend(loc=0)
+plt.xlim(99, 101)
 plt.show()
 
 
