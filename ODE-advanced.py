3d9418402cfb73cd3ee71506122e608bfdca1e5e
ctriandafillou <ctriandafillou@uchicago.edu>
2015-10-21 11:15:03 -0500

    ODE arr temp scaling

---
+++ ODE-advanced.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 10:03:45 2015
+
+@author: cat
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from matplotlib import gridspec
+
--- ODE-basic.py
+++ ODE-basic.py
@@ -14,30 +14,31 @@ from scipy.integrate import odeint
 from matplotlib import gridspec
 
 def deriv(z, t):
-    k1 = .05 # Deactivation
-    k2 = .003 # Reactivation
-    k3 = .07 # Protein synthesis
-    k4 = .03 # Protein degradation
+    Ea = 25
+    k1 = 4.*np.exp(Ea*(1-(298./T))) # Deactivation
+    k2 = .1 # Reactivation
+    k3 = .7 # Protein synthesis
+    k4 = .5 # Protein degradation
     
-    dPab = -k1*z[0]*T + k2*z[1]*z[2]
-    diPab = -k2*z[1]*z[2] + k1*z[0]*T
+    dPab = -k1*z[0] + k2*z[1]*z[2]
+    diPab = -k2*z[1]*z[2] + k1*z[0]
     dC = k3*z[1] - k4*z[2]    
     
     return np.array([dPab, diPab, dC])
 
-T = 1                 
-time1 = np.arange(0, 40, .1)
+T = 298                 
+time1 = np.arange(0, 10, .1)
 zinit = np.array([90., 10., 50])
 z1 = odeint(deriv, zinit, time1)
 Tlist = [T, T]
 
-T = 1
-time2 = np.arange(40, 100, .1)
+T = 316
+time2 = np.arange(10, 20, .1)
 z2 = odeint(deriv, z1[-1], time2)
 Tlist.append(T)
 
-T = 1
-time3 = np.arange(100, 180, .1)
+T = 298
+time3 = np.arange(20, 30, .1)
 z3 = odeint(deriv, z2[-1], time3)
 Tlist.append(T)
 
@@ -70,7 +71,7 @@ gs = gridspec.GridSpec(2, 1, height_ratios=[1, 4])
 ax2 = plt.subplot(gs[0])
 ax2.step([time1[0], time1[-1], time2[-1], time3[-1]], Tlist, c='k')
 ax2.set_ylabel('$\Delta$ T (K)')
-ax2.set_ylim(-1, 30)
+ax2.set_ylim(290, 320)
 plt.setp(ax2.get_xticklabels(), visible=False)
 ax = plt.subplot(gs[1], sharex=ax2)
 for i in xrange(3):
