7100b4621471559a133ee0ba11966479068ff611
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2016-01-05 20:38:28 -0600

    before rename
    ODE-v4-mRNA-decay

--- ODE-v4-mRNA-decay.py
+++ ODE-v4-mRNA-decay.py
@@ -14,7 +14,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 from matplotlib import gridspec
-#import time
 
 
 # Parameters from von der Haar 2008
@@ -23,19 +22,21 @@ HSP104_deg = 0.011363 # min^-1
 total_Pab1 = 100115 # unshocked conditions
 #Pab1_deg = 0.000891 # min^-1; basically 0 for our purposes
 base_HSP104_mRNA = 4.7
+total_cellular_mRNA = 12200 #*
+estimated_HSP104_hs = 250 #molecules/cell
 
 
 def deriv(z, t):
-    Ea = 10. # assembly reaction activation energy (arb. units for now)
-    Ea2 = 120. # mRNA production activation energy (arb. units)
-    k1 = .1*np.exp(Ea*(1-(303./T))) # Deactivation (M^-1*min^-1)
-    k2 = .000003 # Reactivation 
-    k3 = .35 # Protein synthesis M^-1*min^-1
+    Ea = 75. # assembly reaction activation energy (arb. units for now)
+    Ea2 = 25. # mRNA production activation energy (arb. units)
+    k1 = .045*np.exp(Ea*(1-(303./T))) # Deactivation (M^-1*min^-1)
+    k2 = .000002 # Reactivation 
+    k3 = 5 # Protein synthesis M^-1*min^-1
     #k4 = HSP104_deg # Protein degradation
-    k4 = 0.02 # very close to the (log growth) determined value, for what it's worth 
+    k4 = 0.03 #Protein degradation
     k5 = 10.*np.exp(Ea2*(1-(303./T))) # mRNA production rate (min^-1)
-    k6 = .000001 # Pab-mRNA binding rate M^-2*min^-1
-    km6 = .01 # Pab-mRNA unbinding rate M^-1*min^-1
+    k6 = .003 # Pab-mRNA binding rate M^-2*min^-1
+    km6 = .03 # Pab-mRNA unbinding rate M^-1*min^-1
     k7 = .01 # mRNA decay rate    
     
     Pab = z[0]
@@ -57,19 +58,19 @@ def deriv(z, t):
     return np.array([dPab, diPab, dC, dmRNAC, dPab_mRNAC, dmRNAB, dPab_mRNAB])
 
 
-T = 317  
-time1 = np.arange(0, 1.0, .001)
+T = 315 
+time1 = np.arange(0, 1.0, .01)
 zinit = np.array([total_Pab1, 0, total_HSP104, 5, 0, 12200, 0])
 z1 = odeint(deriv, zinit, time1)
 Tlist = [T, T]
 
-T = 317
-time2 = np.arange(1.0, 10.0, .001)
+T = 315
+time2 = np.arange(1.0, 10.0, .01)
 z2 = odeint(deriv, z1[-1], time2)
 Tlist.append(T)
 
 T = 303
-time3 = np.arange(10.0, 180.0, .001)
+time3 = np.arange(10.0, 180.0, .01)
 z3 = odeint(deriv, z2[-1], time3)
 Tlist.append(T)
 
@@ -94,5 +95,6 @@ for i in xrange(7):
     ax.plot(times, final[:, i], label=names[i], c=colors[i], linewidth=2)
 ax.set_xlabel('time (min)')
 ax.set_ylabel('Species Count')
-ax.legend(loc=0, fontsize=15)
+ax.set_yscale('log')
+ax.legend(loc='upper right', fontsize=10)
 plt.tight_layout()
