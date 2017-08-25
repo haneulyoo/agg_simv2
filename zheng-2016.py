6a0b959b58d6f5b336b62d2159ff0ed5949aec8a
Cat Triandafillou <ctriandafillou@uchicago.edu>
2017-08-25 12:42:27 -0500

    finished zhen model

--- zheng-2016.py
+++ zheng-2016.py
@@ -17,7 +17,7 @@ k4 = 0.0464 # per min HSP70:UP off rate
 k5 = 4.64e-7 # degradation rate of HSP70:UP
 beta = 1.778 # Transcription activation rate
 Kd = 0.0022 # Dissociation constant of HSF1-DNA interaction
-kdil = 0 # Dilution rate of YFP
+kdil = 2.78e-3 # Dilution rate of YFP; assuming 50% growth rate
 n = 3 # Hill coefficient
 
 
@@ -60,7 +60,7 @@ z = []
 
 for t in T:
     UP_0 = 0.0024*np.exp(0.215*t) # generates initial value of UP; empirical
-    time = np.arange(0, 80, 0.1)
+    time = np.arange(0, 200, 0.1)
     zinit = np.array([HSP_0, HSF1_0, HSP_HSF1_0, HSP_UP_0, UP_0, YFP_0])
     z.append(odeint(deriv, zinit, time))
 
@@ -69,7 +69,7 @@ for t in T:
 names = ['37', '39', '40', '42', '45']
 colors = ['royalblue', 'firebrick', 'gold', 'darkgreen', 'k']
 
-f = plt.figure(figsize=(5,6))
+f = plt.figure(figsize=(10,5))
 ax = f.add_subplot(111)
 for i in range(len(z)):
     ax.plot(time, z[i][:,5], color = colors[i], label = names[i])
