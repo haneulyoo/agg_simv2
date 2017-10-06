ad67a9938d36f33ed66cc935965946f255574969
Cat Triandafillou <ctriandafillou@uchicago.edu>
2017-10-06 13:55:41 -0500

    T dependence of z model

--- zheng-2016.py
+++ zheng-2016.py
@@ -10,14 +10,15 @@ from scipy.integrate import odeint
 
 
 # Parameters
-k1 = 166.8 # per min per au, client:hsp70 off rate
+k1 = 166.8 # per min per au, client:hsp70 on rate
 k3 = k1
 k2 = 2.783 # per min, HSP70:HSF1 off rate
 k4 = 0.0464 # per min HSP70:UP off rate
 k5 = 4.64e-7 # degradation rate of HSP70:UP
 beta = 1.778 # Transcription activation rate
 Kd = 0.0022 # Dissociation constant of HSF1-DNA interaction
-kdil = 2.78e-3 # Dilution rate of YFP; assuming 50% growth rate
+#kdil = 2.78e-3 # Dilution rate of YFP; assuming 50% growth rate
+kdil = 0 # Dilution rate of YFP assuming no growth
 n = 3 # Hill coefficient
 
 
@@ -54,7 +55,6 @@ def deriv(z, t):
 
 
 
-# Inital test: T = 39C
 T = [37, 39, 40, 42, 45]
 z = []
 
@@ -74,28 +74,56 @@ ax = f.add_subplot(111)
 for i in range(len(z)):
     ax.plot(time, z[i][:,5], color = colors[i], label = names[i])
 ax.legend(title="Temperature")
+ax.set_yscale("log")
+ax.set_ylabel("Reporter level")
+ax.set_xlabel("Time (min)")
 plt.title("Reporter level vs. time")
 
 
-f = plt.figure(figsize=(10,5))
-ax = f.add_subplot(111)
-for i in range(len(z)):
-    ax.plot(time, z[i][:,2], color = colors[i], label = names[i])
-ax.legend(title="Temperature")
-plt.title("HSP70-HSF1 complex concentration vs. time")
+#f = plt.figure(figsize=(10,5))
+#ax = f.add_subplot(111)
+#for i in range(len(z)):
+#    ax.plot(time, z[i][:,2], color = colors[i], label = names[i])
+#ax.legend(title="Temperature")
+#plt.title("HSP70-HSF1 complex concentration vs. time")
 
 
 f = plt.figure(figsize=(10,5))
 ax = f.add_subplot(111)
 for i in range(len(z)):
-    ax.plot(time, z[i][:,3]+z[i][:,4], color = colors[i], label = names[i])
+    ax.plot(time, z[i][:,4], color = colors[i], label = names[i])
 ax.legend(title="Temperature")
-plt.title("total unfolded protein vs. time")
+ax.set_ylabel("Free client")
+ax.set_xlabel("Time (min)")
+plt.title("Free client vs. time")
 
 
+#f = plt.figure(figsize=(10,5))
+#ax = f.add_subplot(111)
+#for i in range(len(z)):
+#    ax.plot(time, z[i][:,0]+z[i][:,2]+z[i][:,3], color = colors[i], label = names[i])
+#    ax.set_yscale("log")
+#ax.legend(title="Temperature")
+#plt.title("total HSP70 vs. time")
+
+
+#Plotting time to zero unfolded protein as a function of temperature
+T = np.linspace(35, 46)
+times = []
+
+for t in T:
+    UP_0 = 0.0024*np.exp(0.215*t) # generates initial value of UP; empirical
+    time = np.arange(0, 200, 0.1)
+    zinit = np.array([HSP_0, HSF1_0, HSP_HSF1_0, HSP_UP_0, UP_0, YFP_0])
+    x = odeint(deriv, zinit, time) # run the simulation
+    times.append(np.min(np.where(x[:,4] < 1)))
+    
+    
 f = plt.figure(figsize=(10,5))
 ax = f.add_subplot(111)
-for i in range(len(z)):
-    ax.plot(time, z[i][:,0]+z[i][:,2]+z[i][:,3], color = colors[i], label = names[i])
-ax.legend(title="Temperature")
-plt.title("total HSP70 vs. time")
+ax.plot(T, times)
+ax.set_xlabel("Shock temperature (C)")
+ax.set_ylabel("Time to clear unfolded protein/clients (min)")
+ax.set_yscale("log")
+ax.annotate("[P](0) = 0.0024*exp(0.215*temp)", (35, 500))
+
